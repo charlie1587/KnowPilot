c164e70fae78c05b6b4b7c1765cfd76901769d64
Chuanqi Tang <cta156@sfu.ca>
2025-04-13 23:49:08 -0700

    style: Modify the format of config.py

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,7 @@ import yaml
 
 CONFIG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "config", "config.yaml"))
 
-with open(CONFIG_PATH, "r") as f:
+with open(CONFIG_PATH, "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
 
 db_config = config["database"]
