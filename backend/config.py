cb70d2271ea56cfcae2a920a0f19a7be2769801d
Chuanqi Tang <cta156@sfu.ca>
2025-04-13 22:38:24 -0700

    feat(backend): Add config file

---
+++ backend/config.py
@@ -0,0 +1,13 @@
+import os
+import yaml
+
+CONFIG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "config", "config.yaml"))
+
+with open(CONFIG_PATH, "r") as f:
+    config = yaml.safe_load(f)
+
+db_config = config["database"]
+DB_ENGINE = db_config.get("engine", "sqlite")
+SQLITE_DB_PATH = os.path.abspath(db_config["sqlite_path"])
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{SQLITE_DB_PATH}"
+SQL_ECHO = db_config.get("echo", False)
